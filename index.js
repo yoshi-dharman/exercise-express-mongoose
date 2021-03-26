9706118bd87b307ed0a01fec21a80b4ab481e20f
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-03-27 01:30:23 +0700

    add info main page

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(routes);
 
 app.get('/', (req, res) => {
     res.send("<h1 style='text-align:center; padding-top:3rem'>Welcome to my Api Inventory ⊂◉‿◉つ</h1>"+
-    "<h3 style='text-align:center;'>/user to see the data</h3>")
+    "<h3 style='text-align:center;'>/all to see the data relation (only GET ALL)</h3>"+
+    "<h3 style='text-align:center;'>/user to see the data user</h3>"+
+    "<h3 style='text-align:center;'>/todo to see the data todo</h3>")
 })
 
 app.get("/all", (req, res) => {
